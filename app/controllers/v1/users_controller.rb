5ecad1c7847c07af57d25be419199743f054b3ec
Tabish Raza <tabishraza@gmail.com>
2017-12-30 11:18:05 +0000

    crud for user

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -3,6 +3,27 @@ class V1::UsersController < ApplicationController
   def index
     @users = User.all
     render json: @users, status: :ok
-   end
+  end
 
+  def create
+    @user = Contact.new(user_params)
+    @user.save
+    render json: @user, status: :created
+  end
+
+
+  def destroy
+    @user = User.where(id: params[:id]).first
+    if @user.destroy
+      head[:ok]
+    else
+      head(:unprocessable_entity)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password)
+  end
 end
